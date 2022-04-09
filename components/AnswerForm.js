10905fad98b0372352599e8032d9c07919da3428
Melvin <contactmelvinadu@gmail.com>
2022-04-09 18:47:09 -0400

    confirm and sign answer message via account/wallet before posting data to database

--- components/AnswerForm.js
+++ components/AnswerForm.js
@@ -1,23 +1,27 @@
 import { useState } from "react"
+import { web3 } from "../lib/web3"
 
 const AnswerForm = function ({ accounts, setAnswers, isLoggedIn }) {
   const [message, setMessage] = useState("")
 
   const post = async function (event) {
     event.preventDefault()
-  
 
     // TODO!
     // send the message state to the /api/answers
     // but we need to verify who we say we are!
     // we don't want people pretending it's us!
 
+    const confirmationMessage = "This message is to verify that you are the person posting this reply!"
+
+    const signedMessage = await web3.eth.personal.sign(confirmationMessage, accounts[0])
+
     const data = { 
       questionId: 1,
       reply: message, 
       account: accounts[0],
-    //   confirmationMessage: _____,
-    //   signedMessage: _____
+      confirmationMessage: confirmationMessage,
+      signedMessage: signedMessage
     }
 
     fetch("/api/answers", { 
