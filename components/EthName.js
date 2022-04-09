36a6444fd045b8d6a8783c7f1efa8a2377ed3c11
Melvin <contactmelvinadu@gmail.com>
2022-04-09 18:00:05 -0400

    add jazzicon for each ETH account

--- components/EthName.js
+++ components/EthName.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react"
 import { web3 } from '../lib/web3';
 
+import Jazzicon, { jsNumberForAddress } from 'react-jazzicon'
+
 const EnsName = function ({ address }) {
   // TODO!
   // get the address from outside
@@ -11,19 +13,24 @@ const EnsName = function ({ address }) {
 
   let formattedAddress = address.substr(0, 8) + "..." + address.substr(-4);
 
+  let icon = (
+    <Jazzicon diameter={32} seed={jsNumberForAddress(address)} />
+  )
+
   return (
     <div className="eth-name">
       <div className="icon">
         {/* icon goes here */}
+        {icon}
       </div>
 
       <div className="name">
         <span className="primary">
-          {formattedAddress}
           {/* ENS name if one here */}
         </span>
         <span className="secondary">
           {/* formatted address here */}
+          {formattedAddress}
         </span>
       </div>
      
